b6a653afd03adcf96a920c80119bb0aeb2f01d24
AmancMat <mateus_amancio@outlook.com>
2020-08-03 14:32:29 -0300

    0.0.0.2.0 Montagem da tela para escolha padrão do orçamento

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -4,7 +4,7 @@ import 'home_controller.dart';
 
 class HomePage extends StatefulWidget {
   final String title;
-  const HomePage({Key key, this.title = "Home"}) : super(key: key);
+  const HomePage({Key key, this.title = ""}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
@@ -20,8 +20,109 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
         title: Text(widget.title),
       ),
       body: Column(
-        children: <Widget>[],
+        children: <Widget>[
+          SingleChildScrollView(
+            child: Center(
+              child: Column(
+                children: [
+                  Container(
+                    color: Colors.blue[900],
+                    height: MediaQuery.of(context).size.height *.08,
+                    width: MediaQuery.of(context).size.width *.8,
+                    margin: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height *.18,
+                    ),
+                    padding: EdgeInsets.only(
+                      left: MediaQuery.of(context).size.height *.02,
+                      top: MediaQuery.of(context).size.height *.026,
+                    ),
+                    child: Text(
+                        'Escolha um padrão para seu orçamento',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  ),
+                  Container(
+                    color: Colors.blue[900],
+                    height: MediaQuery.of(context).size.height *.4,
+                    width: MediaQuery.of(context).size.width *.8,
+                    margin: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height *.005,
+                    ),
+                    child: Center(child: MyDropDownWidget()),
+                  ),
+                  Container(
+                    color: Colors.grey[600],
+                    height: MediaQuery.of(context).size.height *.08,
+                    width: MediaQuery.of(context).size.width *.8,
+                    child: RaisedButton(
+                      child: Text('Continuar',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22
+                      ),
+                      ),
+                        onPressed: (null)),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
+class MyDropDownWidget extends StatefulWidget {
+  MyDropDownWidget({Key key}) : super(key: key);
+
+  @override
+  _MyDropDownWidgetState createState() => _MyDropDownWidgetState();
+}
+
+class _MyDropDownWidgetState extends State<MyDropDownWidget> {
+  String dropdownValue = 'Desenvolvedor Mobile';
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: Icon(Icons.arrow_downward,
+        color: Colors.white,
+      ),
+      dropdownColor: Colors.black87,
+      iconSize: 24,
+      elevation: 16,
+      style: TextStyle(color: Colors.deepPurple),
+      onChanged: (String newValue) {
+        setState(() {
+          dropdownValue = newValue;
+        });
+      },
+      items: <String>[
+        'Desenvolvedor Mobile',
+        'Web Designer',
+        'Interior Designer',
+        'Digital Illustrator']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 16,
+                color: Colors.white,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+        );
+      }).toList(),
+    );
+  }
+}
+
