e6359b88028a065ea2567533a273e35bb2b2b3b0
AmancMat <mateus_amancio@outlook.com>
2020-08-26 17:40:03 -0300

    0.0.0.3.1 Correção de cor e título da appbar na screen "gerar pdf"

--- lib/app/modules/pdf/pdf_page.dart
+++ lib/app/modules/pdf/pdf_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_full_pdf_viewer/flutter_full_pdf_viewer.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:orcamento_mestre/app/modules/pdf/pdf_controller.dart';
 import 'package:provider/provider.dart';
 
@@ -11,10 +12,16 @@ class PagePDF extends StatefulWidget {
 class _PagePDFState extends State<PagePDF> {
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.getInstance()..init(context);
     final pdfController = Provider.of<PdfController>(context);
     return PDFViewerScaffold(
         appBar: AppBar(
-          title: Text("Document"),
+          backgroundColor: Colors.blue[900],
+          title: Text("Seu Orçamento está pronto!",
+            style: TextStyle(
+              fontSize: ScreenUtil.instance.setSp(50),
+            ),
+          ),
           actions: <Widget>[
             IconButton(
               icon: Icon(Icons.share),
